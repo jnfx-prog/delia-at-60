54c1733c378a50760a05196a091622c3aa5ad048
jnfx-prog <jnfx2427@gmail.com>
2026-03-07 17:32:36 +0800

    script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById('rsvpBtn').addEventListener('click', function () {
 
     // --- GOOGLE FORM CONFIGURATION ---
     // 1. Replace the URL below with your Form's "viewform" URL, but change "viewform" to "formResponse"
-    const formURL = "https://docs.google.com/forms/u/0/d/e/YOUR_FORM_ID_HERE/formResponse";
+    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSe-bYTF0CB08vVxn38CoooKF43357qKcNB4uu-dDByYTMJUZA/viewform?usp=preview";
 
     // 2. Replace 'entry.XXXXX' with your actual Entry ID
     const entryID = "entry.118511384";
@@ -43,3 +43,4 @@ document.getElementById('rsvpBtn').addEventListener('click', function () {
         });
 
 });
+
